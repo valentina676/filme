db8eef027140d6ebcd3a2985e3679fba585ccba2
valentina676 <153191667+valentina676@users.noreply.github.com>
2024-09-06 11:27:14 -0300

    filme0609

---
+++ database/migrations/2024_09_06_135807_create_filmes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('filmes', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nome',255);
+            $table->string('sinopse',255);
+            $table->string('ano',255);
+            $table->string('categoria',255);
+            $table->string('imagem',255);
+            $table->string('link',255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('filmes');
+    }
+};
